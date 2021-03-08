8cd01a382ea21b0246b8e730d25048b09b368a99
Yaret BP <helliox2014@gmail.com>
2021-03-07 18:26:22 -0600

    Init API

---
+++ DatingApp/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Entities
+{/// <summary>
+/// App user entity
+/// </summary>
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
